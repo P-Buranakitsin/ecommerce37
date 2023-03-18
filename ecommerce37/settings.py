742023a0cdfdde751492f3c741c05b1bac3e5613
Pavaruth Buranakitsin <2646802b@student.gla.ac.uk>
2023-03-18 19:46:17 +0000

    Added context_processors to count cart item

--- ecommerce37/settings.py
+++ ecommerce37/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'app.context_processors.cart_item_count',
             ],
         },
     },
